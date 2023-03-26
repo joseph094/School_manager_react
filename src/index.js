b279b4fca43c9b8de0f2be7c1dfd6ec61e940981
AwSomZ <awsomz21@gmail.com>
2023-03-26 21:49:49 +0100

    Changer mot de passe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import UpdatePassword from "./components/UpdatePassword/UpdatePassword";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UpdatePassword />
   </React.StrictMode>
 );
