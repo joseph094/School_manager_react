5c5e8ebf1877e702e1ce4f6be5d89a47542e7a8c
AwSomZ <awsomz21@gmail.com>
2023-04-02 21:56:16 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Router, Routes, Route } from 'react-router-dom'
+import { Router} from 'react-router-dom'
 import CreateEvent from "./components/CrudEvents/CreateEvent";
 import ListEvents from "./components/CrudEvents/ListEvents";
 import UpdateEvent from "./components/CrudEvents/updateEvent";
@@ -8,6 +8,7 @@ import UpdateEvent from "./components/CrudEvents/updateEvent";
 import CreateTeacher from "./components/CrudEns/CreateEnseig";
 import ListEnseignants from "./components/CrudEns/ListEnseignents";
 import UpdateEns from "./components/CrudEns/updateEns";
+import SignupAlumni from "./components/SignUp/SignUp";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Route path='/Events' element={<ListEvents />} />
         <Route path='/newEvent' element={<CreateEvent />} />
         <Route path="/Events/:id" element={<UpdateEvent />} />
-
+        <Route path="/signup" element={<SignupAlumni />} />
         <Route path="/enseignants" element={<ListEnseignants />} />
         <Route path="/NewEnseignant" element={<CreateTeacher />} />
         <Route path="/enseignants/:id" element={<UpdateEns />} />
