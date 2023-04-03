96effc38cfc52a105cdf674c8ab3fb6b55ca7cfa
Houcem Eddine Mabrouki <Houcem Eddine Mabrouki>
2023-04-04 00:41:45 +0100

    routes +

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 
-import EtudiantCRUD from "./components/etudiant/etudiantCRUD";
 import InsertStageEte from "./components/etudiant/insertStageEte";
 function App() {
-  // return <EtudiantADD />;
-  // return <Signin />;
-  // return <EtudiantPUT />;
-  return <EtudiantCRUD />;
+  return (
+    <Routes>
+      <Route path="/getetudiant" element={<GetEtudiant />} />
+      <Route path="/modifyetudiants" element={<EtudiantPUT />} />
+      <Route path="/ajouteretudiant" element={<EtudiantADD />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/insertstage" element={<InsertStageEte />} />
+      <Route path="/insertpfe" element={<InsertPFE />} />
+      <Route path="/getalletudiant" element={<GetallEtudiants />} />
+    </Routes>
+  );
 }
 
 export default App;
