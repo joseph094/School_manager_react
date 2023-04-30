2cff428fde9d5d72cf64402f48f6f08a6dab9199
Houcem Eddine Mabrouki <Houcem Eddine Mabrouki>
2023-05-01 00:56:41 +0100

    sideBar token problem

--- src/App.js
+++ src/App.js
@@ -20,28 +20,54 @@ import UpdatePassword from "./components/UpdatePassword/UpdatePassword";
 import AlumniAccountState from "./components/AlumniAccountState/AlumniAccountState";
 import GetUnverifiedAlumniAccounts from "./components/ValidateAlumniAccount/GetUnverifiedAlumniAccounts";
 import VerifyAlumniAccount from "./components/ValidateAlumniAccount/VerifyAlumniAccount";
+import SideBar from "./components/SideBar/SideBar";
+import Choisirpfe from "./components/pfe/choisirpfe";
+import Getpfe from "./components/pfe/getpfe";
+import GetpfeByEnseignantId from "./components/pfe/getpfeByEnseignantID";
+import PfeStats from "./components/pfe/pfeStats";
+import EnseignantDashboard from "./components/enseignantDashboard";
+import GetEtudiantByParam from "./components/etudiant/getetudiantbyParam";
+import GetPfeByParam from "./components/pfe/getPfeByParam";
 function App() {
   return (
-    <Routes>
-      <Route path="/Events" element={<ListEvents />} />
-      <Route path="/newEvent" element={<CreateEvent />} />
-      <Route path="/Events/:id" element={<UpdateEvent />} />
-      <Route path="/signup" element={<SignupAlumni />} />
-      <Route path="/enseignants" element={<ListEnseignants />} />
-      <Route path="/NewEnseignant" element={<CreateTeacher />} />
-      <Route path="/enseignants/:id" element={<UpdateEns />} />
-      <Route path="/getetudiant" element={<GetEtudiant />} />
-      <Route path="/modifyetudiants" element={<EtudiantPUT />} />
-      <Route path="/ajouteretudiant" element={<EtudiantADD />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/alumnistatus" element={<AlumniAccountState />} />
-      <Route path="/getunverified" element={<GetUnverifiedAlumniAccounts />} />
-      <Route path="/valider/:id" element={<VerifyAlumniAccount />} />
-      <Route path="/passreset" element={<UpdatePassword />} />
-      <Route path="/insertstage" element={<InsertStageEte />} />
-      <Route path="/insertpfe" element={<InsertPFE />} />
-      <Route path="/getalletudiant" element={<GetallEtudiants />} />
-    </Routes>
+    <Container>
+      <LeftSide>{/* <SideBar /> */}</LeftSide>
+      <RightSide>
+        <Routes>
+          <Route path="/Events" element={<ListEvents />} />
+          <Route path="/newEvent" element={<CreateEvent />} />
+          <Route path="/Events/:id" element={<UpdateEvent />} />
+          <Route path="/signup" element={<SignupAlumni />} />
+          <Route path="/enseignants" element={<ListEnseignants />} />
+          <Route path="/NewEnseignant" element={<CreateTeacher />} />
+          <Route path="/enseignants/:id" element={<UpdateEns />} />
+          <Route path="/getetudiant" element={<GetEtudiant />} />
+          <Route path="/modifyetudiants" element={<EtudiantPUT />} />
+          <Route path="/ajouteretudiant" element={<EtudiantADD />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/alumnistatus" element={<AlumniAccountState />} />
+          <Route
+            path="/getunverified"
+            element={<GetUnverifiedAlumniAccounts />}
+          />
+          <Route path="/valider/:id" element={<VerifyAlumniAccount />} />
+          <Route path="/passreset" element={<UpdatePassword />} />
+          <Route path="/insertstage" element={<InsertStageEte />} />
+          <Route path="/insertpfe" element={<InsertPFE />} />
+          <Route path="/getalletudiant" element={<GetallEtudiants />} />
+          <Route path="/getpfe" element={<Getpfe />} />
+          <Route path="/getpfeenseignant" element={<GetpfeByEnseignantId />} />
+          <Route path="/getpfeStats" element={<PfeStats />} />
+          <Route
+            path="/enseignantdashboard"
+            element={<EnseignantDashboard />}
+          />
+          <Route path="/choisirpfe" element={<Choisirpfe />} />
+          <Route path="/etudiant/:id" element={<GetEtudiantByParam />} />
+          <Route path="/pfe/:id" element={<GetPfeByParam />} />
+        </Routes>
+      </RightSide>
+    </Container>
   );
 }
 
@@ -50,17 +76,17 @@ export default App;
 const Container = styled.div`
   width: 100%;
   display: flex;
-  height:100vh;
+  height: 100vh;
   flex-direction: row;
 `;
 
 const LeftSide = styled.div`
-transition: all ease-in .3s;
-z-index:99;
+  transition: all ease-in 0.3s;
+  z-index: 99;
 `;
 
 const RightSide = styled.div`
-width:100%;
-overflow-y: auto;
-overflow-x: hidden;
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
